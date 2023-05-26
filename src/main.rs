2cf74030c3dcd5439dcbd4af13614509f8b929bb
tonymushah <tonyramanantsalama@gmail.com>
2023-05-26 07:28:51 +0300

    added simple search

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use rawkuma_scraper::RawKumaClient;
 #[tokio::main]
 async fn main() {
     let mut client = RawKumaClient::default();
-    let home = client.chapter(&"spy-x-family-chapter-80").await.unwrap();
-    let mut file_ = File::create("tests/test_data3.json").unwrap();
+    let home = client.search(&"konsei").await.unwrap();
+    let mut file_ = File::create("tests/test_data4.json").unwrap();
     file_.write(format!("{}", serde_json::to_string(&(home)).unwrap()).as_bytes()).unwrap();
 }
